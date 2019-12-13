57396a698bc2de048ef54493cedba50e089ab893
Gabriela possari <gabrielapossari@hotmail.com>
2019-12-13 10:11:16 -0300

    13/12

--- RoleTOPmvc/ViewModels/HistoricoViewModel.cs
+++ RoleTOPmvc/ViewModels/HistoricoViewModel.cs
@@ -6,6 +6,10 @@ namespace RoleTOPMVC.ViewModels
 {
     public class HistoricoViewModel : BaseViewModel
     {
+        public Evento evento {get;set;}
         public List<Evento> Evento {get;set;}
+        public HistoricoViewModel() {
+            this.Evento = new List<Evento>();
+        }
     }
 }
